d56c904196dae2a6aefb0722477323bb3a6ac07b
Haris Osmanagic <haris@meroxa.io>
2023-05-12 18:52:07 +0200

    specs

---
+++ src/main/java/io/conduit/connectors/generator/GeneratorSpec.java
@@ -0,0 +1,30 @@
+package io.conduit.connectors.generator;
+
+import io.conduit.sdk.Specification;
+
+public class GeneratorSpec implements Specification {
+    @Override
+    public String name() {
+        return "generator-java";
+    }
+
+    @Override
+    public String summary() {
+        return "generator connector, written in java";
+    }
+
+    @Override
+    public String description() {
+        return summary();
+    }
+
+    @Override
+    public String version() {
+        return "v0.1.0";
+    }
+
+    @Override
+    public String author() {
+        return "Meroxa, Inc.";
+    }
+}
